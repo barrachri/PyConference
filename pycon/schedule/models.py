b2626373125c477ada4303b910d55c3a2eb5a006
Christian Barra <barrachri@gmail.com>
2015-11-18 12:56:11 +0100

    added conference model

--- pycon/schedule/models.py
+++ pycon/schedule/models.py
@@ -38,14 +38,59 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 
 class Conference(models.Model):
-    '''
-    Speaker is linked with a registered user, so every speaker must be a user
-    '''
-    name = models.CharField(max_length=50)
-    year = models.BooleanField(default=False)
-    duration = models.TextField(blank=True)
-    url = models.URLField(blank=True)
-    twitter_link = models.URLField(blank=True)
+    name = models.CharField(max_length=100)
+    cfp_start = models.DateField(null=True, blank=True)
+    cfp_end = models.DateField(null=True, blank=True)
+    conference_start = models.DateField(null=True, blank=True)
+    conference_end = models.DateField(null=True, blank=True)
+    voting_start = models.DateField(null=True, blank=True)
+    voting_end = models.DateField(null=True, blank=True)
+
+    def days(self):
+        output = []
+        if self.conference_start and self.conference_end:
+            d = self.conference_start
+            step = datetime.timedelta(days=1)
+            while d<= self.conference_end:
+                output.append(d)
+                d += step
+        return output
+
+    def clean(self):
+        if self.conference_start and self.conference_end:
+            if self.conference_start > self.conference_end:
+                raise exceptions.ValidationError('range di date per la conferenza non valido')
+        if self.cfp_start and self.cfp_end:
+            if self.cfp_start > self.cfp_end:
+                raise exceptions.ValidationError('range di date per il cfp non valido')
+        if self.voting_start and self.voting_end:
+            if self.voting_start > self.voting_end:
+                raise exceptions.ValidationError('range di date per la votazione non valido')
+
+    def cfp(self):
+        today = datetime.date.today()
+        try:
+            return self.cfp_start <= today <= self.cfp_end
+        except TypeError:
+            # date non impostate
+            return False
+
+    def voting(self):
+        today = datetime.date.today()
+        try:
+            return self.voting_start <= today <= self.voting_end
+        except TypeError:
+            # date non impostate
+            return False
+
+    def conference(self):
+        today = datetime.date.today()
+        try:
+            return self.conference_start <= today <= self.conference_end
+        except TypeError:
+            raise
+            # date non impostate
+            return False
 
     class Meta:
         ordering = ['name']
